15712db0804ee259be3af0821bcb3f18d595b06c
Toad <thaddeus.claassen@gmail.com>
2018-02-20 13:38:27 -0600

    made money_request and float after getting it in form

--- update/views.py
+++ update/views.py
@@ -119,7 +119,7 @@ class CreateWorkView(TemplateView):
                 jobuser.save();
             description = form.cleaned_data['description'];
             type = form.cleaned_data['type'];
-            money_request = form.cleaned_data['money_request'];
+            money_request = float(form.cleaned_data['money_request']);
             update = Update(jobuser=jobuser, description=description, request_money=money_request, work_status=type, random_string=createRandomString());
             jobuser.work_status = type;
             if (type == 'work'):
