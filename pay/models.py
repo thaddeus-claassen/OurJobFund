6568b8fcb29ae9870481c91d181d896d15678e07
Toad <thaddeus.claassen@gmail.com>
2018-02-20 14:50:11 -0600

    Trying blank=True in pay/models to see if it fixes OperationalError in production server.

--- pay/models.py
+++ pay/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User;
 from jobuser.models import JobUser;
     
 class Pay(models.Model):
-    sender_jobuser = models.ForeignKey(JobUser, on_delete=models.CASCADE, related_name='sender_jobuser', null=True);
-    receiver_jobuser = models.ForeignKey(JobUser, on_delete=models.CASCADE, related_name='receiver_jobuser', null=True);
+    sender_jobuser = models.ForeignKey(JobUser, on_delete=models.CASCADE, related_name='sender_jobuser', null=True, blank=True);
+    receiver_jobuser = models.ForeignKey(JobUser, on_delete=models.CASCADE, related_name='receiver_jobuser', null=True, blank=True);
     type = models.CharField(choices=(('Other', 'Other'), ('Credit', 'Credit')), max_length=10);
     amount = models.FloatField(editable=False, null=True, blank=True);
     date = models.DateTimeField(auto_now_add=True);
